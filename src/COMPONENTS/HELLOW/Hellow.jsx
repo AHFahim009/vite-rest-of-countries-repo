9fb5a85b0e92d109abe1727ec41c8e22d8f39687
Ahfahim009 <ahfba009@gmail.com>
2023-03-24 16:11:29 +0600

    change

---
+++ src/COMPONENTS/HELLOW/Hellow.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hellow = () => {
+  return (
+    <div>
+      <h1>I am checking how to use github with netlify ......</h1>
+    </div>
+  );
+};
+
+export default Hellow;
