690ffcc8146c0ca7a8ba334b010e3d41be796197
Ahfahim009 <ahfba009@gmail.com>
2023-03-24 16:24:42 +0600

    working

--- src/COMPONENTS/HELLOW/Hellow.jsx
+++ src/COMPONENTS/HELLOW/Hellow.jsx
@@ -4,6 +4,9 @@ const Hellow = () => {
   return (
     <div>
       <h1>I am checking how to use github with netlify ......</h1>
+      <h2>
+        Alhamdulliah It works....It helps me to do fast project and thinking....
+      </h2>
     </div>
   );
 };
